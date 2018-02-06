06264451eca3c47fb2b55a451fff63251546c7ed
Izbatt <izbatt@gmail.com>
2018-02-06 13:44:35 -0600

    Dockerized flask app running on port 5000 that returns any json from /api

---
+++ web/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+@app.route('/api', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return '', 200
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
